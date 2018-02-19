a090aa9ce57e237462edaf133fa494f08fd780ef
akiltipu <akilmahmod.tipu@gmail.com>
2018-02-19 12:32:00 +0600

    Upadate code style

--- chap9/9.7.c
+++ chap9/9.7.c
@@ -1,24 +1,33 @@
 /***********************************
  *Computer Programming: Part-1
  *Chapter 9: String
- *Example: 9.5
+ *Example: 9.7 (Concatenate String basic)
  *Author: Md Akil Mahmod Tipu
- *Email: amtipu.bb@gmail.com
+ *contact: www.akiltipu.com
  
  ***********************************/
 #include <stdio.h>
+
 int main ()
 {
     char str1[] = "Bangla", str2[] = "desh", str3[12];
+    
     int i, j, length1 = 6, length2 = 4;
-    for(i = 0, j = 0; i < length1; i++, j++){
+    
+    for(i = 0, j = 0; i < length1; i++, j++)
+    {
         str3[j] = str1[i];
     }
-    for(i = 0; i <length2; i++, j++){
+    //printf("The value of j is %d\n", j);
+    
+    for(i = 0; i <length2; i++, j++)
+    {
         str3[j] = str2[i];
 
     }
+    
     str3[j] = '\0';
+    
     printf("%s\n", str3);
 
     return 0;
